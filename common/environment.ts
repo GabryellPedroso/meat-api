6ba326819e233f328ec9734b4845e31d1ee9fc5a
Gabryell Corrêa Pedroso <gabryell.correa.15@gmail.com>
2019-11-10 19:59:27 -0300

    fix: DB url
    Changed the db name

--- common/environment.ts
+++ common/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   server: { port: process.env.SERVER_PORT || 3000 },
-  db: { url: process.env.DB_URL || 'mongodb+srv://gabryellcorrea:o2lT5wcxfOdbp9Oe@development-m10la.gcp.mongodb.net/test?retryWrites=true&w=majority' }
+  db: { url: process.env.DB_URL || 'mongodb+srv://gabryellcorrea:o2lT5wcxfOdbp9Oe@development-m10la.gcp.mongodb.net/meat-api?retryWrites=true&w=majority' }
 }
