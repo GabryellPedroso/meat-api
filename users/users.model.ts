aa6cbeda5163f7f5cafbe31b209e76c899d91e57
Gabryell Corrêa Pedroso <gabryell.correa.15@gmail.com>
2019-11-10 20:03:21 -0300

    feat: Users resource using DB
    - "find" instead "findAll" to get users from DB
    - User db schema to set user properties

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,31 +1,16 @@
-const users = [
-  {
-    id: '1',
-    name: 'Peter Parker',
-    email: 'peter@marvel.com'
+import * as mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String
   },
-  {
-    id: '2',
-    name: 'Bruce Wayne',
-    email: 'bruce@dc.com'
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
   }
-]
+})
 
-export class User {
-  static findAll(): Promise<any[]> {
-    return Promise.resolve(users)
-  }
-
-  static findById(id: string): Promise<any> {
-    return new Promise((resolve) => {
-      const filtered = users.filter((user) => user.id === id)
-      let user = undefined
-
-      if (filtered.length > 0) {
-        user = filtered[0]
-      }
-
-      resolve(user)
-    })
-  } 
-}
+export const User = mongoose.model('User', userSchema)
--- users/users.router.ts
+++ users/users.router.ts
@@ -6,7 +6,7 @@ class UsersRouter extends Router {
   applyRoutes(application: restify.Server) {
     
     application.get('/users', (req, resp, next) => {
-      User.findAll().then((users) => {
+      User.find().then((users) => {
         resp.json(users)
         return next()
       })
