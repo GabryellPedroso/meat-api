535bcd6fd4ca5a0382a439b3c3c0d0350d4926ad
Gabryell Corrêa Pedroso <gabryell.correa.15@gmail.com>
2019-11-10 15:37:56 -0300

    feat: Restify first exemple
    - import restify
    - restify create server function
    - get route
    - server listen port 3000

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+import * as restify from 'restify'
+
+const server = restify.createServer({
+  name: 'meat-api',
+  version: '1.0.0'
+})
+
+server.get('/hello', (req, resp, next) => {
+  resp.json({ message: 'Hello!' })
+  return next()
+})
+
+server.listen(3000, () => {
+  console.log('API is running on http://localhost:3000')
+})
