9d9151bf55ecec4229a608f0a5f77542b3a233b7
Gabryell Corrêa Pedroso <gabryell.correa.15@gmail.com>
2019-11-10 20:45:18 -0300

    feat: Users PUT method
    - overwrite option to replace all user properties inside DB

--- users/users.router.ts
+++ users/users.router.ts
@@ -32,6 +32,21 @@ class UsersRouter extends Router {
         return next()
       })
     })
+
+    application.put('/users/:id', (req, resp, next) => {
+      const options = { overwrite: true }
+      User.update({ _id: req.params.id }, req.body, options)
+          .exec().then((result) => {
+            if (result.n) {
+              return User.findById(req.params.id)
+            } else {
+              resp.send(404)
+            }
+          }).then((user) => {
+            resp.json(user)
+            return next()
+          })
+    })
   
   }
 }
