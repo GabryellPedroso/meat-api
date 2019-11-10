70fac25d200731edc56bda457fff9f213415ee28
Gabryell Corrêa Pedroso <gabryell.correa.15@gmail.com>
2019-11-10 16:48:42 -0300

    feat: Router class and users resources
    - router class that is used to apply routes in the server
    - server bootstrap router params
    - param "routers" in server initRoutes function
    
    For each router passed through bootstrap function it will apply the router in the server sending the restify.Server to the common router file

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
 import * as restify from 'restify'
 import { environment } from '../common/environment'
+import { Router } from '../common/router'
 
 export class Server {
   application: restify.Server
 
-  initRoutes(): Promise<any> {
+  initRoutes(routers: Router[]): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
         this.application = restify.createServer({
@@ -15,33 +16,9 @@ export class Server {
 
 
         // Routes
-        this.application.get('/info', [
-          (req, resp, next) => {
-            if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-              // resp.status(400)
-              // resp.json({ message: 'Please, update your browser' })
-        
-              let error: any = new Error()
-              error.statusCode = 400
-              error.message = 'Please, update your browser'
-              return next(error)
-            }
-            return next()
-          },
-          (req, resp, next) => {
-            // resp.contentType = 'application/json'
-            // resp.setHeader('Content-Type', 'application/json')
-        
-            resp.json({
-              browser: req.userAgent(),  // "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"
-              method: req.method,
-              url: req.href(),
-              path: req.path(),
-              query: req.query
-            })
-            return next()
-          }
-        ])
+        for (let router of routers) {
+          router.applyRoutes(this.application)
+        }
 
 
         this.application.listen(environment.server.port, () => {
@@ -53,7 +30,7 @@ export class Server {
     })
   }
 
-  bootstrap(): Promise<Server> {
-    return this.initRoutes().then(() => this)
+  bootstrap(routers: Router[] = []): Promise<Server> {
+    return this.initRoutes(routers).then(() => this)
   }
 }
---
+++ users/users.router.ts
@@ -0,0 +1,16 @@
+import * as restify from 'restify'
+import { Router } from '../common/router'
+import { User } from './users.model'
+
+class UsersRouter extends Router {
+  applyRoutes(application: restify.Server) {
+    application.get('/users', (req, resp, next) => {
+      User.findAll().then((users) => {
+        resp.json(users)
+        return next()
+      })
+    })
+  }
+}
+
+export const usersRouter = new UsersRouter()
